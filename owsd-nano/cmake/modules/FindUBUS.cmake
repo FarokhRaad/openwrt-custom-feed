8ca28c38181304bda099e686d1dffa97477a2ad0
FarokhRaad <raad.farokh@gmail.com>
2025-07-03 17:57:56 +0330

    owsd-nano: initial OpenWrt packaging
    - Add Makefile for owsd-nano minimal version (forked from owsd, upstream: https://dev.iopsys.eu/iopsys/owsd)
    - Remove DBus and other optional features for reduced footprint
    - Add explicit dependencies: libwebsockets, ubus, ubox, libjson-c
    - Add minimal init and config files
    - Set clear attribution to original and fork maintainers

---
+++ owsd-nano/cmake/modules/FindUBUS.cmake
@@ -0,0 +1,18 @@
+find_package(PkgConfig)
+pkg_check_modules(PC_UBUS QUIET ubus)
+
+find_path(UBUS_INCLUDE_DIR ubus.h
+  HINTS ${PC_UBUS_INCLUDEDIR} ${PC_UBUS_INCLUDE_DIRS}
+)
+
+find_library(UBUS_LIBRARY ubus
+  HINTS ${PC_UBUS_LIBDIR} ${PC_UBUS_LIBRARY_DIRS}
+)
+
+set(UBUS_LIBRARIES ${UBUS_LIBRARY})
+set(UBUS_INCLUDE_DIRS ${UBUS_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(UBUS DEFAULT_MSG UBUS_LIBRARY UBUS_INCLUDE_DIR)
+
+mark_as_advanced(UBUS_INCLUDE_DIR UBUS_LIBRARY)
